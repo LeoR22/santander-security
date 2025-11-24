e0acad98ec2adc84192b5c26ab6c920d388cf960
Leandro Rivera <leo.232rivera@gmail.com>
2025-11-24 11:17:27 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 # app/main.py
 from fastapi import FastAPI
-from app.routers import crimes, analytics, chatbot
+from app.routers import analytics, chatbot, crimes, geo
 
-app = FastAPI(title="Santander Security API", version="0.1.0")
-
-app.include_router(crimes.router)
-app.include_router(analytics.router)
-app.include_router(chatbot.router)
+app = FastAPI(title="Santander Security API", version="1.0.0")
 
 @app.get("/health")
 def health():
-    return {"status":"ok"}
+    return {"status": "ok"}
+
+app.include_router(analytics.router)
+app.include_router(crimes.router)
+app.include_router(geo.router)
+app.include_router(chatbot.router)
